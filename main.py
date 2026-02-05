6c696dff4f60f2d4ea2a4745a94ad2a4a2335e88
morrisg4mer <samuelmorris50@gmail.com>
2026-02-05 12:08:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class VideoRequest(BaseModel):
+    youtube_url: str
 
 @app.get("/")
 def home():
     return {"status": "ok"}
 
+@app.post("/processar")
+def processar_video(data: VideoRequest):
+    return {
+        "status": "recebido",
+        "youtube_url": data.youtube_url
+    }
